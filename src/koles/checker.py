3a139c9444df4d9a97a319742ee2d5ed02085bf1
myslak71 <43068450+myslak71@users.noreply.github.com>
2019-07-20 14:35:33 +0200

    Add filename checking

--- src/koles/checker.py
+++ src/koles/checker.py
@@ -1,7 +1,7 @@
 """Koles checker module."""
 import os
 import re
-from typing import Set, Generator, List, Tuple
+from typing import Set, Generator, List
 
 import pkg_resources
 
@@ -10,11 +10,7 @@ class KolesChecker:
     """A koles checker class."""
 
     def __init__(self, path: str) -> None:
-        """
-        Initialize path and word matching pattern.
-
-        :param path: path to check
-        """
+        """Initialize path and word matching pattern."""
         self._path = path
         self._pattern = '|'.join(self._get_bad_words())
 
@@ -29,33 +25,40 @@ class KolesChecker:
         data = pkg_resources.resource_string(__name__, 'data/english.dat')
         return set(data.decode().strip().split('\n'))
 
-    def _check_line(self, line: str) -> List[int]:
+    def _check_string(self, string: str) -> List[int]:
         """Return a list of bad words starting positions."""
         nasty_positions = [
-            m.start() for m in re.finditer(f'(?=({self._pattern}))', line)
+            m.start() for m in re.finditer(f'(?=({self._pattern}))', string)
         ]
+
         return nasty_positions
 
-    def _check_file(self, path: str) -> List[Tuple[int, List[int]]]:
+    def _check_file(self, path: str) -> List[str]:
         """Check the file for inappropriate language and return existing errors."""
         errors = []
+        rude_filename = self._check_string(path)
+
+        if rude_filename:
+            message = f'{path}: Filename contains bad language at position: {rude_filename}'
+            errors.append(message)
 
         with open(path, encoding='utf-8') as file:
             for line_number, line in enumerate(file, 1):
-                error_positions = self._check_line(line)
+                error_positions = self._check_string(line)
                 if error_positions:
-                    errors.append((line_number, error_positions))
+                    errors.append(
+                        f'{path}:{line_number}: Inappropriate vocabulary found at position: {error_positions}'
+                    )
 
         return errors
 
     def check(self):
         """Check the given path and return formatted errors."""
-        errors = []
+        errors: List[str] = []
         files_to_check = self._get_files_to_check()
+
         for file in files_to_check:
             result = self._check_file(file)
-            errors += [
-                f'{file}:{line_number}: Inappropriate vocabulary found at positions: {positions}'
-                for line_number, positions in result
-            ]
+            errors += result
+
         return '\n'.join(errors)
