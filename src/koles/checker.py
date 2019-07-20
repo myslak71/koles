8b98fc6f3421d5c808ccc7897b9b092ad1cc922d
myslak71 <>
2019-07-20 18:18:05 +0200

    Refactor

--- src/koles/checker.py
+++ src/koles/checker.py
@@ -39,24 +39,37 @@ class KolesChecker:
 
         return nasty_positions
 
-    def _check_file(self, path: str) -> List[str]:
-        """Check the file for inappropriate language and return existing errors."""
+    def _check_file_content(self, path: str) -> List[str]:
+        """Check the file and return formatted errors."""
         errors = []
-        file_error = self._check_string(path)
-
-        if file_error:
-            errors.append(
-                f'{path}: Filename contains bad language at position: {file_error}'
-            )
 
         with open(path, encoding='utf-8') as file:
-            for number, line in enumerate(file, 1):
+            for line_number, line in enumerate(file, 1):
                 positions = self._check_string(line)
                 if positions:
                     errors.append(
-                        f'{path}:{number}: '
+                        f'{path}:{line_number}: '
                         f'Inappropriate vocabulary found at position: {positions}'
                     )
+
+        return errors
+
+    def _check_file(self, path: str) -> List[str]:
+        """Check the file for inappropriate language and return errors."""
+        errors = []
+        filename = self._check_string(path)
+
+        if filename:
+            errors.append(
+                f'{path}: Filename contains bad language at position: {filename}'
+            )
+
+        try:
+            file_errors = self._check_file_content(path)
+            if file_errors:
+                errors += file_errors
+        except UnicodeDecodeError as e:
+            return [f'{path}: File couldn\'t have been opened: {e}']
 
         return errors
 
