805cd1ad6d29cdb2fa1fe79404c72f3d37823a5c
myslak71 <43068450+myslak71@users.noreply.github.com>
2019-07-23 22:48:59 +0200

    Refactor

--- src/koles/config.py
+++ src/koles/config.py
@@ -6,10 +6,12 @@ from koles.errors import OptionValidationError
 
 DEFAULT_CONFIG = {
     'ignore-shorties': 0,
+    'exit-code': False
 }
 
 OPTION_VALIDATION_MAP = {
     'ignore-shorties': None,
+    'exit-code': False
 }
 
 
@@ -19,23 +21,30 @@ class KolesConfig(UserDict):
     ALLOWED_CONFIG_FILENAMES = ('setup.cfg',)
     DEFAULT_CONFIG = DEFAULT_CONFIG
 
-    def __init__(self, path, run_dir: str) -> None:
+    def __init__(self, cli_args, run_dir: str) -> None:
         """Set path and config."""
-        self.path = path
+        self.path = cli_args.path
+        self._cli_args = {
+            key: value for key, value in vars(cli_args).items()
+            if key != 'path' and value is not None
+        }
         self._run_dir = run_dir
+
         super().__init__(self._get_file_config())
 
     def _discover_config_file_path(self) -> str:
         """Discover config file and return its path."""
         #  config filename is hard-coded for now
-        return f'{self._run_dir}/{self.ALLOWED_CONFIG_FILENAMES[0]}'
+        return None
 
     def _get_file_config(self) -> dict:
         """Set config."""
         path = self._discover_config_file_path()
 
         if not path:
-            return self.DEFAULT_CONFIG
+            data = DEFAULT_CONFIG.copy()
+            data.update(self._cli_args)
+            return data
 
         config = configparser.ConfigParser()
         config.read(path)
